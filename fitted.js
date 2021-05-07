1e96edcec77a8b9f279222f47749040f910c6e0a
Aidan Shirey <aidan.shirey@gmail.com>
2021-05-07 02:25:00 -0700

    Sock Select Added + Function Templates

---
+++ fitted.js
@@ -0,0 +1,12 @@
+var fitpath = "";
+function addSocks(cardid){
+    document.getElementById("outfitpreview").setAttribute("src", document.getElementById(cardid).getAttribute("src"));
+}
+
+function addPants(cardid){
+
+}
+
+function addShirt(cardid){
+
+}
