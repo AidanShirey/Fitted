73e5c5f30504e7a87c4622e65dd4d85e463766a0
Aidan Shirey <aidan.shirey@gmail.com>
2021-05-08 21:49:41 -0700

    Added Some Tab Functionality

--- fitted.js
+++ fitted.js
@@ -1,12 +1,23 @@
 var fitpath = "";
-function addSocks(cardid){
+function addItem(cardid){
+    // Change Outfit Preview Window
     document.getElementById("outfitpreview").setAttribute("src", document.getElementById(cardid).getAttribute("src"));
 }
 
-function addPants(cardid){
-
-}
-
-function addShirt(cardid){
-
+function changeTab(tabid){
+    // Change Item Select Cards
+    switch(tabid){
+        case "socks":
+            document.getElementById("card1").setAttribute("src","Clothing/black-socks.png");
+            document.getElementById("card2").setAttribute("src","Clothing/grey-socks.png");
+            document.getElementById("card3").setAttribute("src","Clothing/white-socks.png");
+            document.getElementById("card4").setAttribute("src","Clothing/khaki-socks.png");
+            break;
+        case "pants":
+            document.getElementById("card1").setAttribute("src","Clothing/black-pants.png");
+            document.getElementById("card2").setAttribute("src","Clothing/grey-pants.png");
+            document.getElementById("card3").setAttribute("src","Clothing/green-pants.png");
+            document.getElementById("card4").setAttribute("src","Clothing/blue-pants.png");
+            break;
+    }
 }
